5725c1212ce72873ba891a21e85b37c0b5aba24e
TakutoHashimoto <tkthsmt0531@gmail.com>
2024-01-29 02:41:37 +0900

    Add print_number.c

---
+++ ch2/print_number.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("今年の西暦は%d年です。\n", 2024);
+    printf("フルマラソンのコースは%.3fキロです。\n", 42.195);
+    printf("%d歳男子の平均身長は%.1fcmです。\n", 10, 138.9);
+    return 0;
+}
